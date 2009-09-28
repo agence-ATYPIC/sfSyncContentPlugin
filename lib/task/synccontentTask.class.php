3b63fb51c0471c9f3d194e23b0512620d435d48a
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-09-28 22:10:16 +0000

    Databases are now synced using dedicated mysql-load and mysql-dump tasks which are executed remotely via ssh. This eliminates the need for buggy re-parsing of databases.yml and drastically cuts down on the number of password prompts required. A big improvement. Support for syncing a specific database connection by name has been removed as this was never well-tested. However in the future I'm looking at supporting syncing either all databases or just the first one, the two likely scenarios.
    git-svn-id: http://svn.symfony-project.com/plugins/sfSyncContentPlugin/trunk@22583 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/synccontentTask.class.php
+++ lib/task/synccontentTask.class.php
@@ -23,19 +23,19 @@ class sfSyncContentTask extends sfBaseTask
         'The application name ("frontend")'),
       new sfCommandArgument('localenv', 
         sfCommandArgument::REQUIRED, 
-        'The local environment and optional database connection name ("dev" or "dev:doctrine")'),
+        'The local environment ("dev")'),
       new sfCommandArgument('direction', 
         sfCommandArgument::REQUIRED, 
         'Either "from" or "to"; when you specify "from" content is copied FROM the remote site, when you specify "to" content is copied TO the remote site'),
       new sfCommandArgument('remoteenv',
         sfCommandArgument::REQUIRED, 
-        'The remote environment and site, with an optional database connection name ("dev@staging" or "prod@production:propel"). The site name must be defined in properties.ini')));
+        'The remote environment and site. The site name must be defined in properties.ini')));
 
     $this->namespace        = 'project';
     $this->name             = 'sync-content';
     $this->briefDescription = 'Synchronize content (not code) between Symfony instances';
     $this->detailedDescription = <<<EOF
-You must specify the application ("frontend"), local environment and an optional database connection name ("dev" or "dev:doctrine"), "to" or "from", and the remote database connection name, environment and site ("dev@staging:doctrine", "prod@production:doctrine", etc). If you do not specify a database connection name the first (usually only) database connection in databases.yml for the specified environment is used. In addition to the database, data folders listed at app_sfSyncContentPlugin_content will also be synced.',
+You must specify the application ("frontend"), local environment ("dev"), "to" or "from", and the remote environment and site ("dev@staging", "prod@production", etc). Currently only the default database connection is copied. This may change in a future release. In addition to the database, data folders listed at app_sfSyncContentPlugin_content will also be synced.',
 
 EOF;
   }
@@ -51,7 +51,7 @@ EOF;
 
     if (count($args) != 5)
     {
-      throw new sfException('You must specify the application ("frontend"), the local environment and an optional database connection name ("dev" or "dev:doctrine"), "to" or "from", and the remote database connection name, environment and site ("dev@staging:doctrine", "prod@production:doctrine", etc). If you do not specify a database connection name the first (usually only) database connection in databases.yml for the specified environment is used.');
+      throw new sfException('You must specify the application ("frontend"), the local environment ("dev"), "to" or "from", and the remote environment and site ("dev@staging", "prod@production", etc). Currently only the first database is copied. Later versions may copy all databases.');
     }
     
     $settings = parse_ini_file("config/properties.ini", true);
@@ -67,17 +67,12 @@ EOF;
       throw new sfException("The third argument must be either 'to' or 'from'");
     }  
 
-    if (!preg_match('/^(.*)\@(.*)(:(\w+))?$/', $args['remoteenv'], $matches))
+    if (!preg_match('/^(.*)\@(.*)?$/', $args['remoteenv'], $matches))
     {
-      throw new sfException("Fourth argument must be of the form environment@site with an optional database connection name, example: dev@staging or dev@staging:propel; the site must be defined in properties.ini");
+      throw new sfException("Fourth argument must be of the form environment@site, example: dev@staging or prod@production; the site must be defined in properties.ini");
     }
     $envRemote = $matches[1];
     $site = $matches[2];
-    $connectionNameRemote = $matches[4];
-    if (!isset($connectionNameRemote))
-    {
-      $connectionNameRemote = false;
-    }
     
     $found = false;
     foreach ($settings as $section => $data)
@@ -91,19 +86,14 @@ EOF;
 
     if (!$found)
     {
-      throw new sfException("Fourth argument must be of the form environment@site with an optional database connection name, example: dev@staging or dev@staging:propel; the site must be defined in properties.ini");
+      throw new sfException("Fourth argument must be of the form environment@site example: dev@staging the site must be defined in properties.ini");
     }
 
     if (!preg_match('/^(\w+)(:(\w+))?$/', $args['localenv'], $matches))
     {
-      throw new sfException("Second argument must be an environment name followed, optionally, by a database connection name. Example #1: dev Example #2: prod:propel");
+      throw new sfException("Second argument must be an environment name. Example #1: dev Example #2: prod");
     }
     $env = $matches[1];
-    $connectionName = $matches[2];
-    if (!isset($connectionName))
-    {
-      $connectionName = false;
-    }
     $pathLocal = '.';
     $pathRemote = $data['user'] . '@' . $data['host'] . ':' . $data['dir'];
     if (isset($data['port']))
@@ -111,36 +101,24 @@ EOF;
       // Needed in multiple places least hairy this way
       $this->sshPort = $data['port'] + 0;
     }
-    $dbDataLocal = $this->_content_sync_get_db_data($application, $pathLocal, $env, $connectionName);
-    $dbDataRemote = $this->_content_sync_get_db_data($application, $pathRemote, $envRemote, $connectionNameRemote);
-
-    $stamp = date('Y-m-d-H-i-s', time());
-    $cmd = "mysqldump --skip-opt --add-drop-table --create-options " .
-      "--disable-keys --extended-insert --set-charset " . 
-      $this->_content_sync_format_db_credentials(
-      ($direction == 'to') ? $dbDataLocal : $dbDataRemote);
-    if ($direction == 'from')
-    {
-      $cmd = $this->_content_sync_build_remote_cmd($pathRemote, $cmd);
-    }
-    $cmd .= " > sqldump.$stamp"; 
-    $this->_content_sync_system($cmd);
-
-    if ($direction == 'to')
-    {
-      $this->_content_sync_rsync("sqldump.$stamp", $pathRemote . "/sqldump.$stamp");
-    }
-    $cmd = "mysql " . $this->_content_sync_format_db_credentials(
-      ($direction == 'to') ? $dbDataRemote : $dbDataLocal);
     
+    // Drastic simplification due to the new sfDatabaseManager-based approach and
+    // the remote dump and load tasks
+    
+    $params = sfSyncContentTools::shellDatabaseParams(sfSyncContentTools::getDatabaseParams($this->configuration));
+
     if ($direction == 'to')
     {
-      $this->_content_sync_remote_system($pathRemote, "$cmd < sqldump.$stamp");
+      $cmd = "mysqldump --skip-opt --add-drop-table --create-options " .
+        "--disable-keys --extended-insert --set-charset $params | " . $this->_content_sync_build_remote_cmd($pathRemote, "./symfony project:mysql-load --application=$application --env=$envRemote");
+      $this->_content_sync_system($cmd);
     }
     else
     {
-      $this->_content_sync_system("$cmd < sqldump.$stamp");
+      $cmd = $this->_content_sync_build_remote_cmd($pathRemote, "./symfony project:mysql-dump --application=$application --env=$envRemote") . " | mysql $params "; 
+      $this->_content_sync_system($cmd);
     }
+    
     $asData = sfConfig::get('app_sfSyncContent_content',
       array());
     foreach ($asData as $path)
@@ -158,7 +136,6 @@ EOF;
       $to = dirname($to);
       $this->_content_sync_rsync($from, $to);
     }
-    unlink("sqldump.$stamp");
   }
 
   function _content_sync_rsync($path1, $path2)
@@ -202,131 +179,6 @@ EOF;
     }    
     pclose($in);
     return $data;
-  }
-
-  function _content_sync_get_db_data($application, $path, $env, $connectionName = false)
-  {
-    // Symfony 1.2 will use an application-specific databases.yml
-    // if there is one; we need to do that too
-    
-    // This isn't the best error check imaginable; we don't capture
-    // errors from a remote 'cat' command yet. It works for our
-    // limited purposes here
-    try
-    {
-      $data = $this->_content_sync_file_get_contents($path, "apps/$application/config/databases.yml");
-    } catch (Exception $e)
-    {
-      $data = false;
-    }
-    if (!strlen($data))
-    {
-      $data = $this->_content_sync_file_get_contents($path, "config/databases.yml");
-    }
-    $dbSettings = sfYaml::load($data);
-    if (!$dbSettings)
-    {
-      throw new sfException("Unable to load databases.yml from $path");
-    }
-   
-    $dbData = array();
-    if (isset($dbSettings['all']))
-    {
-      $dbData = $dbSettings['all'];
-    }
-    if (isset($dbSettings[$env]))
-    {
-      $dbData = array_merge($dbData, $dbSettings[$env]);
-    }
-    if ($connectionName === false)
-    {
-      foreach ($dbData as $connectionName => $connection)
-      {
-        // Grab the first one
-        break;
-      }
-    }
-    else
-    {
-      if (!isset($dbData[$connectionName]))
-      {
-        throw new sfException("Specified db connection $connectionName does not appear in config/databases.yml");
-      }
-      $connection = $dbData[$connectionName];
-    }
-    if (!strlen($connectionName))
-    {
-      throw new sfException("No db connections configured in config/databases.yml");
-    }    
-    if (!isset($connection['class']))
-    {
-      throw new sfException("db connection without a class attribute encountered");
-    }
-    if ($connection['class'] === 'sfPropelDatabase')
-    {
-      if (isset($connection['param']['dsn']))
-      {
-        if (!preg_match("/^mysql\:\/\/(.*)?:(.*)?\@(.*?)\/(.*?)\s*$/", $dbData['propel']['param']['dsn'], $matches))
-        {
-          throw new sfException("dsn is incorrect or not a MySQL dsn");
-        }
-        $dbData = array();
-        list($dummy, $user, $password, $host, $database) = $matches;
-      } 
-      elseif (isset($connection['param']['username']))
-      {
-        if ($connection['param']['phptype'] != 'mysql')
-        {
-          throw new sfException("phptype is not set to MySQL");
-        }  
-        $user = $connection['param']['username'];
-        $password = $connection['param']['password'];
-        $host = $connection['param']['host'];
-        $database = $connection['param']['database'];
-      }
-      else
-      {
-        throw new sfException("There is no dsn or username setting for $env or all in properties.ini");
-      }  
-    }
-    elseif ($connection['class'] === 'sfDoctrineDatabase')
-    {
-      if (!isset($connection['param']['dsn']))
-      {
-        throw new sfException("dsn is not set");
-      }
-      $dsn = $connection['param']['dsn'];
-      if (!preg_match("/^mysql\:(.*)$/", $dsn, $matches))
-      {
-        throw new sfException("dsn must be a MySQL dsn");
-      }
-      $rawArgs = explode(";", $matches[1]);
-      $args = array();
-      foreach ($rawArgs as $rawArg)
-      {
-        list($key, $val) = explode("=", $rawArg);
-        $args[$key] = $val;
-      }
-      if (!isset($args['dbname']))
-      {
-        throw new sfException("dsn has no dbname");
-      }
-      if (!isset($args['host']))
-      {
-        throw new sfException("dsn has no host");
-      }
-      $database = $args['dbname'];
-      $host = $args['host'];
-      $user = $connection['param']['username']; 
-      $password = $connection['param']['password']; 
-    }
-
-    return array(
-      "user" => $user,
-      "password" => $password,
-      "host" => $host,
-      "database" => $database
-    );
   }
 
   function _content_sync_build_remote_cmd($pathRemote, $cmd)
